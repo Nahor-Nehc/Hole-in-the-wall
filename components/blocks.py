cc27afd0fbd2e94e8f39da24b665b6ab557da958
Nahor Nehc <90894291+Nahor-Nehc@users.noreply.github.com>
2023-06-20 16:01:53 +0100

    - added movement - fixed collision rects

--- components/blocks.py
+++ components/blocks.py
@@ -1,5 +1,5 @@
 from pygame import Rect
-from pygame.draw import rect
+from pygame.draw import rect as draw_rect
 from collections.abc import MutableSequence
 
 class Block(Rect):
@@ -17,22 +17,37 @@ class Block(Rect):
     else:
       raise ValueError("Collision rect percentage not between 0 and 1")
     
-    self.collision_rect =Rect(self.left + self.width*(collision_rect_percentage/2),
-                              self.top + self.height*(collision_rect_percentage/2),
-                              self.width * collision_rect_percentage,
-                              self.height * collision_rect_percentage)
+    collision_rect_left = int(self.left + self.width*((1-collision_rect_percentage)/2))
+    collision_rect_top = int(self.top + self.height*((1-collision_rect_percentage)/2))
+    collision_rect_width = int(self.width * collision_rect_percentage)
+    collision_rect_height = int(self.height * collision_rect_percentage)
+    
+    self.collision_rect =Rect(collision_rect_left,
+                              collision_rect_top,
+                              collision_rect_width,
+                              collision_rect_height)
+    
+    print(self.collision_rect)
   
-  def draw(self, WIN):
-    rect(WIN, self.colour, self)
+  def draw(self, WIN, see_collision_box):
+    draw_rect(WIN, self.colour, self)
+    if see_collision_box == True:
+      draw_rect(WIN, (255, 255, 0), self.collision_rect, 1)
+
   
-  def move(self, x_direction, y_direction):
+  def move_generic(self, x_direction, y_direction):
     self.x += x_direction
     self.y += y_direction
+    self.collision_rect.x += x_direction
+    self.collision_rect.y += y_direction
   
+  def move(self):
+    self.move_generic(0, self.speed)
 
 class Blocks(MutableSequence):
   def __init__(self):
     self.blocks = []
+    self.see_collision_box = False
   
   # def __call__(self):
   #   return self.blocks
@@ -57,7 +72,17 @@ class Blocks(MutableSequence):
   
   def remove(self, block:Block):
     self.blocks.remove(block)
+
+  def toggle_see_collision_box(self):
+    self.see_collision_box = not self.see_collision_box
   
   def draw(self, WIN):
     for block in self.blocks:
-      block.draw(WIN)
+      block.draw(WIN, self.see_collision_box)
+      
+  def move(self):
+    for block in self.blocks:
+      block.move()
+  
+  def create(self, left, top, width, height, colour, speed, collision_rect_percentage:float):
+    new_block = Block(left, top, width, height, colour, speed, collision_rect_percentage, self)
