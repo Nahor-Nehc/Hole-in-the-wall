ca4979100f3e3daa11031a0d13c4b512106f67e0
Nahor Nehc <90894291+Nahor-Nehc@users.noreply.github.com>
2023-07-09 14:47:04 +0100

    bounded player movement

--- main.py
+++ main.py
@@ -25,6 +25,15 @@ DBROWN = (159, 100,  64)
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Hole in the wall")
 
+# sizes
+SIZE = 50
+PLAYER_BOUNDS = {
+  "top": HEIGHT/3*2 - SIZE*2,
+  "bottom": HEIGHT/3*2 + SIZE*2,
+  "left": WIDTH/2 - SIZE*2,
+  "right": WIDTH/2 + SIZE*2
+}
+
 # fonts
 FONT = lambda x: pygame.font.SysFont("consolas.ttf", x)
 TITLEFONT = FONT(70)
@@ -34,7 +43,7 @@ from os import path
 PATH_TO_ATLAS_IMAGE = path.join("assets", "images", "atlas.bmp")
 PATH_TO_LEVELS = path.join("assets", "levels", "levels")
 
-def handle_events(player, mouse):
+def handle_events(player, mouse, state):
   for event in pygame.event.get():
     #if the "x" button is pressed ...
     if event.type == pygame.QUIT:
@@ -50,23 +59,54 @@ def handle_events(player, mouse):
       import sys
       sys.exit()
       
-    elif event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_LEFT:
-        player.move(-50, 0)
-      elif event.key == pygame.K_RIGHT:
-        player.move(50, 0)
-      elif event.key == pygame.K_UP:
-        player.move(0, -50)
-      elif event.key == pygame.K_DOWN:
-        player.move(0, 50)
+    if state.get_state() == "game":
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+          player.move(-1*SIZE, 0)
+        elif event.key == pygame.K_RIGHT:
+          player.move(SIZE, 0)
+        elif event.key == pygame.K_UP:
+          player.move(0, -1 * SIZE)
+        elif event.key == pygame.K_DOWN:
+          player.move(0, SIZE)
+        
+        elif event.key == pygame.K_e:
+          state.set_state("editor")
+    
+    elif state.get_state() == "editor":
+      if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_g:
+          state.set_state("game")
 
-def process_game(blocks):
-  blocks.move()
+def process_game(player, blocks, state):
+  if state.get_state() == "game":
+    blocks.move()
+    player.update()
 
-def draw(WIN, player, blocks):
+def draw(WIN, player, blocks, state):
   WIN.fill(BLACK)
-  player.draw(WIN)
-  blocks.draw(WIN)
+  if state.get_state() == "game":
+    player.draw(WIN)
+    blocks.draw(WIN)
+    
+  if state.get_state() == "editor":
+    blocks.draw(WIN)
+    
+    for i in range(0, 5):
+      pygame.draw.line(
+        WIN,
+        WHITE,
+        (PLAYER_BOUNDS["left"] + SIZE*i, 0),
+        (PLAYER_BOUNDS["left"] + SIZE*i, HEIGHT)
+        )
+    
+    for i in range(0, 5):
+      pygame.draw.line(
+        WIN,
+        WHITE,
+        (0, PLAYER_BOUNDS["top"] + SIZE*i),
+        (WIDTH, PLAYER_BOUNDS["top"] + SIZE*i))
+    
   pygame.display.update()
 
 def main():
@@ -83,22 +123,13 @@ def main():
   from components.blocks import Block, Blocks
   
   # GAME VARIABLES
-  state = State("start")
-  player_surface = pygame.Surface((50, 50))
+  state = State("game")
+  player_surface = pygame.Surface((SIZE, SIZE))
   player_surface.fill(WHITE)
-  player = Player(200, 200, player_surface)
+  player = Player(PLAYER_BOUNDS["left"], PLAYER_BOUNDS["top"], player_surface, PLAYER_BOUNDS)
   
   blocks = Blocks()
   blocks.toggle_see_collision_box()
-  
-  blocks.create(100, 100, 50, 50, WHITE, 2, 0.7)
-  blocks.create(200, 100, 100, 50, WHITE, 2, 0.7)
-  
-  blocks.create(100, 0, 50, 50, WHITE, 2, 0.7)
-  blocks.create(200, 0, 100, 50, WHITE, 2, 0.7)
-  
-  blocks.create(100, -100, 100, 50, WHITE, 2, 0.7)
-  blocks.create(250, -100, 50, 50, WHITE, 2, 0.7)
   
   #texture_atlas = TextureAtlas(PATH_TO_ATLAS_IMAGE)
   
@@ -116,10 +147,10 @@ def main():
     mouse = pygame.mouse.get_pos()
     
     #for everything that the user has inputted ...
-    handle_events(player, mouse)
+    handle_events(player, mouse, state)
     
-    process_game(blocks)
+    process_game(player, blocks, state)
     
-    draw(WIN, player, blocks)
+    draw(WIN, player, blocks, state)
 
 main()
