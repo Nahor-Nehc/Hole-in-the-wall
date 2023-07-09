91588adfdf288c9f36a9f70998c45dbbcbd031d5
Nahor Nehc <90894291+Nahor-Nehc@users.noreply.github.com>
2023-07-09 14:46:32 +0100

    bounded player movement

--- components/player.py
+++ components/player.py
@@ -2,17 +2,29 @@ from pygame import Surface, sprite
 from pygame import K_LEFT, K_RIGHT, K_UP, K_DOWN, Vector2
 
 class Player:
-  def __init__(self, x, y, image:Surface):
+  def __init__(self, x, y, image:Surface, bounds):
     sprite.Sprite.__init__(self)
     self.movement = Vector2()
     self.image = image
     self.rect = image.get_rect()
     self.rect.x = x
     self.rect.y = y
+    self.bounds = bounds
   
   def move(self, x_direction, y_direction):
     self.rect.x += x_direction
     self.rect.y += y_direction
+      
+  def update(self):
+    if self.rect.x < self.bounds["left"]:
+      self.rect.x = self.bounds["left"]
+    elif self.rect.right > self.bounds["right"]:
+      self.rect.x = self.bounds["right"] - self.rect.width
+      
+    if self.rect.y < self.bounds["top"]:
+      self.rect.y = self.bounds["top"]
+    elif self.rect.bottom > self.bounds["bottom"]:
+      self.rect.y = self.bounds["bottom"] - self.rect.height
     
   def draw(self, window:Surface):
     window.blit(self.image, (self.rect.x, self.rect.y))
