1a959da64350d3ea354c7ebe5adf796adb40eab4
0uranos <rohanchen00@gmail.com>
2023-07-19 21:50:20 +0100

    added option name and value size

--- components/slider.py
+++ components/slider.py
@@ -12,10 +12,34 @@ class Sliders:
     """call if mouse clicked"""
     for slider in self.sliders:
       slider.update(mouse)
+  
+  def set_visible(self):
+    for slider in self.sliders:
+      slider.visible = True
+    
+  def set_invisible(self):
+    for slider in self.sliders:
+      slider.visible = False
+  
+  def toggle_visibility(self):
+    for slider in self.sliders:
+      slider.visible = not self.visible
+  
+  def get(self, rect):
+    for slider in self.sliders:
+      if slider.rect == rect:
+        return slider
+    return None
 
 class Slider:
-  def __init__(self, min:int, max:int, step:float, rect:Rect, show_buttons = True, draggable_slider = True):
-    """rect is the size and place where all the components of the slider will go"""
+  def __init__(self, min:int, max:int, step:float, rect:Rect, show_buttons = True, draggable_slider = True, top_bar = False, name = "", font = None, value_display = False):
+    """rect is the size and place where all the components of the slider will go
+    
+    top_bar is where name and value display will go"""
+    
+    if top_bar == False and (name != "" or value_display == False):
+      raise ValueError("top_bar not enabled for name and value display")
+    
     self.min = min
     self.max = max
     self.range = max-min
@@ -23,6 +47,9 @@ class Slider:
     
     self.show_buttons = show_buttons
     self.draggable_slider = draggable_slider
+    self.name = name
+    self.font = font
+    self.value_display = value_display
     
     self.rect = rect
     self.surface = Surface((self.rect.width, self.rect.height))
@@ -32,17 +59,26 @@ class Slider:
     self.visible = True
     
     # slider buttons
-    self.buttons_size = self.rect.height
+    if top_bar == False:
+      self.slider_rect = self.rect
+      
+    else:
+      height = self.rect.height // 2
+      self.slider_rect = Rect(self.rect.x, self.rect.y + height, self.rect.width, height)
+    
+    self.slider_surface = Surface((self.slider_rect.width, self.slider_rect.height))
+    
+    self.buttons_size = self.slider_rect.height
     self.button_right = Surface((self.buttons_size, self.buttons_size))
     self.button_left = Surface((self.buttons_size, self.buttons_size))
     
     self.button_right_rect = self.button_right.get_rect()
-    self.button_right_rect.x = self.rect.x + self.surface.get_width() - self.buttons_size
-    self.button_right_rect.y = self.rect.y
+    self.button_right_rect.x = self.slider_rect.x + self.slider_surface.get_width() - self.buttons_size
+    self.button_right_rect.y = self.slider_rect.y
     
     self.button_left_rect = self.button_left.get_rect()
-    self.button_left_rect.x = self.rect.x
-    self.button_left_rect.y = self.rect.y
+    self.button_left_rect.x = self.slider_rect.x
+    self.button_left_rect.y = self.slider_rect.y
     
     draw.line(self.button_right, (255, 255, 255), (self.buttons_size//2, 0), (self.buttons_size//2, self.buttons_size), 3)
     draw.line(self.button_right, (255, 255, 255), (0, self.buttons_size//2), (self.buttons_size, self.buttons_size//2), 3)
@@ -68,24 +104,39 @@ class Slider:
   def draw(self, window):
     if self.visible == True:
       self.surface.fill((0, 0, 0))
-      x = self.rect.x
-      y = self.rect.y
+      self.slider_surface.fill((0, 0, 0))
       
-      # draw slider buttons
-      self.surface.blit(self.button_left, (0, 0))
-      self.surface.blit(self.button_right, (self.surface.get_width() - self.buttons_size, 0))
+      # draw other sections
+      if self.name != "":
+        text = self.font.render(self.name, 1, (255, 255, 255))
+        self.surface.blit(text, (0, 0))
+      
+      if self.value_display == True:
+        text = self.font.render(str(self.current), 1, (255, 255, 255))
+        self.surface.blit(text, (self.button_right_rect.x - text.get_width(), 0))
+      
+      # draw slider section
+      x = self.slider_rect.x
+      y = self.slider_rect.y
+      
+      if self.show_buttons == True:
+        # draw slider buttons
+        self.slider_surface.blit(self.button_left, (0, 0))
+        self.slider_surface.blit(self.button_right, (self.slider_surface.get_width() - self.buttons_size, 0))
       
       # draw slider line
       start_x = self.buttons_size*3//2
-      end_x = self.surface.get_width() - (self.buttons_size*3//2)
-      draw.line(self.surface, (111, 111, 111), (start_x, self.buttons_size//2), (end_x, self.buttons_size//2), 3)
+      end_x = self.slider_surface.get_width() - (self.buttons_size*3//2)
+      draw.line(self.slider_surface, (111, 111, 111), (start_x, self.buttons_size//2), (end_x, self.buttons_size//2), 3)
       
       # draw slider dot
       x_offset = round(((self.current - self.min) / self.range) * (end_x - start_x))
-      draw.circle(self.surface, (255, 255, 255), (start_x + x_offset, self.buttons_size//2 + 1), 4, 1)
+      draw.circle(self.slider_surface, (255, 255, 255), (start_x + x_offset, self.buttons_size//2 + 1), 4, 1)
+      
+      self.surface.blit(self.slider_surface, (0, self.surface.get_height()//2))
       
       # draw slider to window
-      window.blit(self.surface, (x, y))
+      window.blit(self.surface, (self.rect.x, self.rect.y))
   
   def update(self, mouse):
     pos = mouse.get_pos()
