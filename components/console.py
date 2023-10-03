c2fafab6af01dc225308a920e2c81f231508e5cc
0uranos <rohanchen00@gmail.com>
2023-10-03 20:55:06 +0100

    code stubs for console class

---
+++ components/console.py
@@ -0,0 +1,17 @@
+
+class Console:
+  def __init__(self, x, y, width, height, colour, text_colour, font):
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.colour = colour
+    self.text_colour = text_colour
+    self.font = font
+  
+  def draw(self, window):
+    pass
+  
+  def add_text(self, string):
+    pass
+  
